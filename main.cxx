7026477f81fdda2bdeeec6c2dac27917d82fcd2b
KwongYam Lie <1586754446@qq.com>
2023-09-06 22:00:39 +0800

    Changes at Clion

--- main.cxx
+++ main.cxx
@@ -1,7 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-	std::cout<<"Hello Git"<<std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout<< "Hello, CLion"
+             << std::endl;
+    vector<int> v{1,2,3,4,5,6};
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout," "));
 
-	return 0;
+
+
+    return 0;
 }
+
